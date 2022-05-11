341e581e9858994badf2fcc1644acba710449f18
bruvio <bruno.viola@protonmail.com>
2022-05-11 12:25:59 +0000

    Update ecs.tf - container crashes due to low memory

--- deploy/ecs.tf
+++ deploy/ecs.tf
@@ -61,8 +61,8 @@ resource "aws_ecs_task_definition" "api" {
   container_definitions    = data.template_file.api_container_definitions.rendered
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
-  cpu                      = 256
-  memory                   = 512
+  cpu                      = 1024
+  memory                   = 2
   execution_role_arn       = aws_iam_role.task_execution_role.arn
   task_role_arn            = aws_iam_role.app_iam_role.arn
   volume {
