27f9001b9ab3495f414a9c946b4ef6b6d813728d
bruvio <bruno.viola@pm.me>
2022-05-18 15:13:07 +0100

    fix css errors

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 export SYSTEM_ENV='NOPOSTGRES'
 export SECRET_KEY='django-insecure-w=jp7h*^7occppycw4l6a*&ja%#=0_#_qwr=_&krq8e@@m*dkm'
 python manage.py wait_for_db
+python manage.py collectstatic
 python3 manage.py makemigrations API
 python3 manage.py migrate
 # python manage.py flush
--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,6 +3,6 @@
 set -e
 python manage.py wait_for_db
 python manage.py makemigrations API
-python manage.py migrate 
+python manage.py migrate --run-syncdb
 # python import_csv.py
 uwsgi --socket :9000 --workers 4 --master --enable-threads --module traffic_API.wsgi
