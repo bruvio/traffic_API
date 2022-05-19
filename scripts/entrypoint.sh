ab5451120144dc57f589611b053050d23b91a96e
bruvio <bruno.viola@pm.me>
2022-05-19 11:19:24 +0100

    changes to entrypoint - db populate will happen from bastion

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,5 +4,4 @@ set -e
 python manage.py wait_for_db
 python manage.py makemigrations API
 python manage.py migrate
-python manage.py populate_db --num 99 --print true
 uwsgi --socket :9000 --workers 4 --master --enable-threads --module traffic_API.wsgi
